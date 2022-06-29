8552a600bbf0c60312cea26690f8928f9885a3c0
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-06-29 22:44:11 +0800

    Started with login Auth

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import path from 'path';
 
-import __dirname  from './dirname.js';
-import cookieParser  from 'cookie-parser';
-import cors  from 'cors';
-import logger  from 'morgan';
+import __dirname from './dirname.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import logger from 'morgan';
 
-import usersRouter  from './routes/users.js';
+import usersRouter from './routes/users.js';
+
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -15,17 +17,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
-app.use(function (req, res, next) {
-  res.status(404).json({message: "We couldn't find what you were looking for 😞"})
-})
+app.post('/auth/login', (request, response) => {
+  const username = request.body.username;
+});
+
+// app.use(function (req, res, next) {
+//   res.status(404).json({message: "We couldn't find what you were looking for 😞"})
+// })
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json(err)
-})
+  console.error(err.stack);
+  res.status(500).json(err);
+});
 
 export default app;
