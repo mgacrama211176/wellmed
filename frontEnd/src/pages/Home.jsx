8e06ba3bb41ace0f8c4d877744cea050768b23c3
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-06-30 19:17:01 +0800

    Login Button styling added on landing page

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Logo from '../assets/logo.jpg';
 import '../styles/home.css';
 
+//MUI components
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 const Home = () => {
-  const [show, setShow] = useState('none');
-
-  // useEffect(() => {
-  //   setShow(show);
-  // }, [show]);
-
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div>
       <div className="mainContainer">
@@ -24,16 +27,21 @@ const Home = () => {
         <button>Search</button>
       </div>
       <div className="login">
-        <form>
-          <button id="login" onClick={setShow('block')}>
-            Login
-          </button>
-          <div className="inputContainer" style={{ display: `${show}` }}>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <input type="submit" id="submit" />
-          </div>
-        </form>
+        <Button onClick={handleOpen}>Login</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <form>
+            <div className="inputContainer">
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <input type="submit" id="submit" />
+            </div>
+          </form>
+        </Modal>
       </div>
     </div>
   );
