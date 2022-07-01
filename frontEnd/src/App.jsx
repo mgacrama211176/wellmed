b2cd8ccecb459fe73b357427eb89b78570e67ed8
Marlon Gacrama <mgacrama_ccs@uspf.edu.ph>
2022-07-01 23:49:21 +0800

    Nav to console does not work., need help to sir Marvz.

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddItems from './pages/AddItems';
 import Home from './pages/Home';
+import Console from './pages/Console';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<AddItems />} />
+          <Route path="/console" element={<Console />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -1,33 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import Logo from '../assets/logo.jpg';
 import '../styles/home.css';
 
 //MUI components
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 const Home = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  const authAcountAndRedirect = async (e) => {
-    e.preventDefault();
-    console.log(login);
-  };
+  const nav = useNavigate();
+  const url = 'http://localhost:4000/login';
 
   const [login, setLogin] = useState({
     username: '',
     password: '',
   });
 
+  //Auto then Redirect to a new page.,
+  const authAcountAndRedirect = async () => {
+    e.preventDefault();
+    console.log(login);
+    // setLogin('');
+    await axios
+      .post(url, {
+        username: login.username,
+        password: login.password,
+      })
+      .then((response) => {
+        sessionStorage.setItem(
+          'admin',
+          JSON.stringify(login.username, login.password)
+        );
+        nav('/console');
+        console.log(response.data);
+      });
+  };
+
   const onChangeHandle = (e) => {
     const userInput = { ...login };
     userInput[e.target.id] = e.target.value;
     setLogin(userInput);
     console.log(userInput);
+    //Put Notification Below
   };
   return (
     <div>
